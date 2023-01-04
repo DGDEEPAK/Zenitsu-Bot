0f6fb1d42644d05518de42eb29234b89a676bc41
Deepak Gupta <102593178+DGDEEPAK@users.noreply.github.com>
2023-01-04 12:09:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,14 +10,14 @@ global.antitags = true
 //global.autoTyping = false //auto tying in gc (true to on, false to off)
 //global.autoRecord = true //auto recording (true to on, false to off)
 
-global.Owner = ['918348225320']
+global.Owner = ['918348225320'] //ur number 
 global.OwnerNumber = ['918348225320']
 global.ownertag = ['918348225320']
-global.BotName = "ZenitsuBot"
+global.BotName = "ZenitsuBot" //ur bot name 
 global.packname = "ZenitsuBot"
 global.author = "🦄Dream Guy Deepak" //ur sticker pack name 
 global.OwnerName = "🦄Dream Guy Deepak" //ur name
-global.BotSourceCode = "https://youtube.com/@dreamguydeepak"
+global.BotSourceCode = "https://youtube.com/@dreamguydeepak" //ur website link
 global.SupportGroupLink = "https://chat.whatsapp.com/KguOo9XsRTW2iESYuHzVX1" //ur group link 
 global.sessionName = "session"
 
